6479e33574831299c89f415877d032083ea9060b
Aliaksei Kaliutau <aliaksei.kaliutau@gmail.com>
2022-01-18 12:56:38 +0000

    working solution

--- terraform/gae/engine.tf
+++ terraform/gae/engine.tf
@@ -2,8 +2,9 @@
 # GCE Provider
 #####################################################################
 resource "google_service_account" "gce_default_sa" {
-  account_id   = "gce-default-sa"
+  account_id   = "gce-base-sa"
   display_name = "GCE Default Service Account"
+  project = var.project
 }
 
 
--- terraform/gae/template.tf
+++ terraform/gae/template.tf
@@ -3,7 +3,7 @@ module "gce-container" {
   source = "./../gce-container"
 
   container = {
-    image = "gcr.io/message-multi-processor/processor:v1"
+    image = "eu.gcr.io/message-multi-processor/processor:v1"
     env = [
       {
         name = "GOOGLE_CLOUD_PROJECT"
@@ -17,7 +17,7 @@ module "gce-container" {
 
 resource "google_compute_instance_template" "processor_template" {
   name         = "proc-template-1"
-  machine_type = "e2-medium"
+  machine_type = "e2-small"
 
   disk {
     source_image = join("/", [ "cos-cloud", reverse(split("/", module.gce-container.source_image))[0]])
@@ -34,6 +34,9 @@ resource "google_compute_instance_template" "processor_template" {
 
   network_interface {
     network = "default"
+    access_config {
+      network_tier = "PREMIUM"
+    }
   }
 
   metadata_startup_script = "#!/bin/bash\ncurl -sSO https://dl.google.com/cloudagents/install-monitoring-agent.sh\nsudo bash install-monitoring-agent.sh"
@@ -44,8 +47,25 @@ resource "google_compute_instance_template" "processor_template" {
   }
 
   service_account {
-    email = google_service_account.gce_default_sa.email
-    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+#    email = google_service_account.gce_default_sa.email
+    scopes = [
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/pubsub",
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/service.management.readonly",
+      "https://www.googleapis.com/auth/servicecontrol",
+      "https://www.googleapis.com/auth/trace.append",
+      "https://www.googleapis.com/auth/cloudplatformprojects.readonly",
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot = false
+    enable_vtpm = true
   }
 
 
@@ -70,7 +90,7 @@ resource "google_compute_instance_group_manager" "default" {
   }
 
   target_pools       = [google_compute_target_pool.default.id]
-  base_instance_name = "autoscaler-sample"
+  base_instance_name = "proc-instance"
 }
 
 resource "google_compute_autoscaler" "default" {
@@ -90,6 +110,13 @@ resource "google_compute_autoscaler" "default" {
       filter                     = "resource.type = pubsub_subscription AND resource.label.subscription_id = incoming_files"
       target = 2
       type = "GAUGE"
+    }
+
+    scale_down_control {
+      time_window_sec = 300
+      max_scaled_down_replicas {
+        percent = 80
+      }
     }
   }
 }
