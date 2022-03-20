73841ddbf6f6fd3c753f19214641b5e56a68e868
Aliaksei Kaliutau <aliaksei.kaliutau@gmail.com>
2022-03-20 22:33:59 +0000

    added externalization of config

--- terraform/gce/template.tf
+++ terraform/gce/template.tf
@@ -140,7 +140,7 @@ resource "google_compute_autoscaler" "default" {
   target = google_compute_instance_group_manager.default.id
 
   autoscaling_policy {
-    max_replicas    = 4
+    max_replicas    = 3
     min_replicas    = 1
     cooldown_period = 60
 
